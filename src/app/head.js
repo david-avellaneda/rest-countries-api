5978206387acd0d915cf78d2e3ea75c25393483e
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-02-23 09:19:16 -0500

    Structure of global tags contained in <head></head>

---
+++ src/app/head.js
@@ -0,0 +1,19 @@
+const Head = () => {
+  return (
+    <>
+      <title>REST Countries API - David Santiago Avellaneda Montero</title>
+      <meta name="language" content="en" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="robots" content="index,follow" />
+      <meta
+        name="description"
+        content="REST Countries API with color theme switcher"
+      />
+      <meta name="author" content="David Santiago Avellaneda Montero" />
+      <meta name="copyright" content="David Santiago Avellaneda Montero" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+};
+
+export default Head;
