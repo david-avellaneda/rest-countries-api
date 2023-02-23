b1daa7cbdb1798d65cce6fdee78a2110a9954038
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-02-23 09:25:20 -0500

    Font added with next/font

---
+++ src/font/index.js
@@ -0,0 +1,9 @@
+import { Nunito_Sans } from "@next/font/google";
+
+const nunitoSans = Nunito_Sans({
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+  weight: ["300", "600", "800"],
+});
+
+export default nunitoSans;
