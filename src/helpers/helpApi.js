7ca756ea8d2b382d1c0f498a69de022eaf6e5c44
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-03-14 12:26:16 -0500

    Function to make requests to an API

---
+++ src/helpers/helpApi.js
@@ -0,0 +1,24 @@
+export default async function helpApi(path) {
+  const url = `https://restcountries.com/v3.1/${path}`;
+  try {
+    const res = await fetch(url, {
+      next: {
+        // That every 7 days the request is updated, that is, after 7 days the HTML is generated again. 604800 seconds make 7 days
+        revalidate: 604800,
+      },
+    });
+
+    if (!res.ok)
+      throw {
+        err: true,
+        status: res.status || 500,
+        msg: res.statusText || "Internal several error",
+      };
+
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
