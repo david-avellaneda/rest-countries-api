090644c86913bfb4d01564e922b31d1d94dc0a31
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-03-15 14:53:02 -0500

    Detecting errors when obtaining all countries from the API

--- src/components/Countries/index.js
+++ src/components/Countries/index.js
@@ -1,11 +1,33 @@
 import Card from "../Card";
+import Err from "../Err";
 
 const Countries = ({ countries }) => {
   return (
     <>
-      {countries.map((country, index) => (
-        <Card key={index} {...country} i={index} />
-      ))}
+      {countries.length &&
+        countries.map((country, index) => (
+          <Card key={index} {...country} i={index} />
+        ))}
+
+      {countries.err == "region" && (
+        <>
+          <Err
+            msg={
+              "An error has ocurred, the countries could not be filtered by region"
+            }
+            err={countries.err}
+          />
+        </>
+      )}
+
+      {!countries.length && (
+        <>
+          <Err
+            msg={"An error has ocurred, the countries could not be obtained"}
+            err={countries}
+          />
+        </>
+      )}
     </>
   );
 };
