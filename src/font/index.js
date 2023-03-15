9ab9ef0d2420dfc01e7c8e1f6e8c7c22ae138119
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-03-15 08:12:19 -0500

    Added new font weight

--- src/font/index.js
+++ src/font/index.js
@@ -3,7 +3,7 @@ import { Nunito_Sans } from "@next/font/google";
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
   variable: "--font-nunito-sans",
-  weight: ["300", "600", "800"],
+  weight: ["300", "400", "600", "900"],
 });
 
 export default nunitoSans;
