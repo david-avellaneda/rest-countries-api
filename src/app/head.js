451a96042c9709eb457a640da4e03cd6cc0092fc
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2023-03-21 12:11:18 -0500

    Adding Google Analytics

--- src/app/head.js
+++ src/app/head.js
@@ -1,6 +1,21 @@
+import Script from "next/script";
+
 const RootHead = () => {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-RDZGP7KDC1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RDZGP7KDC1');
+        `}
+      </Script>
       <meta name="language" content="en" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="robots" content="index,follow" />
